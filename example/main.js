4b162d0d4accb3968795063cf0f34025cbb900e4
dgkirkwood <daniel.g.kirkwood@gmail.com>
2020-08-26 13:48:47 +1000

    new message

--- example/main.js
+++ example/main.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<p>Hello Snapshots!!</p>',
+    body: '<p>Hello Snapshots!! v2</p>',
   }
   callback(null, response)
 }
