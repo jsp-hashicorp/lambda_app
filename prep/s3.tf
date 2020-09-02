baaa89d5b0c15e97dfcf261f5e55c18ffe2e7003
JS Park <jsp@hashicorp.com>
2020-09-02 22:41:50 +0900

    Create s3.tf

---
+++ prep/s3.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  version = "~> 3.0"
+  region  = "ap-northeast-2"
+}
+
+resource "aws_s3_bucket" "lambda-bucket" {
+  bucket = "jsp-lambda-code-bucket"
+  acl    = "private"
+  
+  tags = {
+   Name = "jsp@hashicorp.com"
+   Environment = "Dev"
+  }
+}
