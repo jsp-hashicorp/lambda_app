3edeac058b24f40576cabee558a537dc8ff9fc80
jsp-hashicorp <jsp@hashicorp.com>
2020-09-03 01:35:48 +0900

    change the way to upload the file

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@ version=$TF_VAR_code_version
 cd example
 zip ../example.zip main.js
 cd ..
-aws s3 cp example.zip s3://jsp-lambda-code-bucket/v${TF_VAR_code_version}/example.zip
+aws s3 cp example.zip s3://jsp-lambda-code-bucket/v${version}/example.zip
