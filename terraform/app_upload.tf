cd708fcc2b8b5c069453be6e02f04c3ebdd4fe2a
jsp-hashicorp <jsp@hashicorp.com>
2020-09-03 00:58:57 +0900

    2nd

--- terraform/app_upload.tf
+++ terraform/app_upload.tf
@@ -13,10 +13,10 @@ resource "null_resource" "zip_file" {
 resource "aws_s3_bucket_object" "object" {
   bucket = "jsp-lambda-code-bucket"
   key    = "v${var.code_version}/example.zip"
-  source = "./example.zip"
+  source = "example.zip"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("./example.zip")
+  etag = filemd5("example.zip")
 }
