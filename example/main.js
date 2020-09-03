36735e5bdc85354e938c66842ee03bb35cfe0fb6
jsp-hashicorp <jsp@hashicorp.com>
2020-09-04 01:15:01 +0900

    v_1_0_5

--- example/main.js
+++ example/main.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<p>Hello 1_0_5</p>',
+    body: '<p>Hello 1_0_5 </p>',
   }
   callback(null, response)
 }
