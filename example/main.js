241b91e7d5610422a2b00df5fc4b69c29b9954f1
jsp-hashicorp <jsp@hashicorp.com>
2020-10-08 10:32:29 +0900

    v2.0.6

--- example/main.js
+++ example/main.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<p>안녕하세요..... 버전은 2_0_5입니다.</p>',
+    body: '<p>안녕하세요..... 버전은 2_0_6입니다.</p>',
   }
   callback(null, response)
 }
