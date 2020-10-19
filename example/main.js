8db2baceecfe91de5d519a55f4cbf3f61d05b4c3
jsp-hashicorp <jsp@hashicorp.com>
2020-10-19 16:37:07 +0900

    v2_1_1

--- example/main.js
+++ example/main.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<p>AWS Serverless Deployment Sample</p><p>안녕하세요..... 버전은 2_0_9입니다.</p>',
+    body: '<p>AWS Serverless Deployment Sample</p><p>안녕하세요..... 버전은 2_1_1입니다.</p>',
   }
   callback(null, response)
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "code_version" {
   type = string
-  default = "2_0_9"
+  default = "2_1_1"
 }
