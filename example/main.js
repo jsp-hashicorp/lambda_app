523259cfcd7f6bd421e9ec527af843807218d20a
jsp-hashicorp <jsp@hashicorp.com>
2022-08-18 15:41:06 +0900

    change version to v2_3_5

--- example/main.js
+++ example/main.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
     headers: {
       'Content-Type': 'text/html; charset=utf-8',
     },
-    body: '<p>AWS Serverless Deployment Sample</p><p>안녕하세요, 현재 버전은 2_3_4입니다.</p>',
+    body: '<h1>AWS Serverless Deployment Sample</h1><h2>안녕하세요, 현재 버전은 2_3_5입니다.</h2>',
   }
   callback(null, response)
 }
